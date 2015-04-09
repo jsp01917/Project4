6330627b2a46fa45b1a2efa3228753fd75785291
Colton S. Berry <coltonberry@uga.edu>
2015-04-08 19:12:01 -0700

    Finished drawing snowman.

--- SnowmanFX.java
+++ SnowmanFX.java
@@ -79,7 +79,33 @@ public class SnowmanFX extends Application {
 		gc.strokeOval(83,10,130,120);
 		gc.fillOval(65,90,170,115);
 		
-	
+		//left eye
+		gc.setFill(Color.BLACK);
+		gc.fillOval(120,40,15,15);
+		//right eye
+		gc.fillOval(160,40,15,15);
+		
+		//nose
+		gc.setFill(Color.ORANGE);
+		double[] xpoints = new double[] {150,150,100};
+		double[] ypoints = new double[] {70,80,75};
+		gc.fillPolygon(xpoints, ypoints, 3);
+		
+		//buttons
+		gc.setFill(Color.GOLD);
+		gc.fillOval(145, 120, 10, 10);
+		gc.fillOval(145, 150, 10, 10);
+		gc.fillOval(145, 180, 10, 10);
+		
+		//left arm
+		gc.setFill(Color.BLACK);
+		gc.fillRect(10, 150, 75, 7.5);
+		gc.fillRect(215, 150, 75, 7.5);
+		
+		//top hat
+		gc.setFill(Color.GREEN);
+		gc.fillRect(97.5, 10, 100, 5);
+		gc.fillRect(110, -60, 75, 75);
 	}
 	
 	
